38c696ec5ab9c85cc49da1309d89ee605334fda0
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-16 17:07:28 +0200

    as

--- lua/rexeg-rename/init.lua
+++ lua/rexeg-rename/init.lua
@@ -1,18 +1,11 @@
 local M = {}
 
 
-local normalMode = require("regex-rename.normalMode")
+-- local normalMode = require("regex-rename.normalMode")
 
 
 function M.rename()
     print("moinsen")
-end
-
-function M.setup(options) 
-    opts = options or {}
-
-    autocmd_group_id = vim.api.nvim_create_augroup("RegexRename", {})
-
 end
 
 return M
