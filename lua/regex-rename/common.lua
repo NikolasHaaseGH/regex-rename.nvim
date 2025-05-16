4013591365121cb4e30e1f4a35b0f64285cecdba
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-17 00:22:38 +0200

    asdf

--- lua/regex-rename/common.lua
+++ lua/regex-rename/common.lua
@@ -15,7 +15,7 @@ local function scanLineForMatches(token, line, matchesArray, lineNumber)
         end
 
         if line[i] == token[match_step] then
-            print(line[i] .. " | " .. token[match_step] .. " | " .. tostring(match_step) .. " | " .. tostring(i))
+            print(line[i] .. " | " .. tostring(match_step) .. " | " .. tostring(i))
             if match_step == tokenLength then
                 table.insert(matchesArray, {lineNumber, match_start})
                 match_start = i+1
