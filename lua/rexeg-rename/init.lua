12d3daada337c794d3f0cd2e640b348ca2ddfbcb
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-16 16:48:31 +0200

    adf

--- lua/rexeg-rename/init.lua
+++ lua/rexeg-rename/init.lua
@@ -9,4 +9,11 @@ function M.rename()
     print("moinsen")
 end
 
+function M.setup(options) 
+    opts = options or {}
+
+    autocmd_group_id = vim.api.nvim_create_augroup("RegexRename", {})
+
+end
+
 return M
