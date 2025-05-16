cd33a27a5caa18ea7d9480d3bcc594b7b18656f2
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-16 23:00:54 +0200

    asdf

--- lua/regex-rename/init.lua
+++ lua/regex-rename/init.lua
@@ -5,7 +5,6 @@ local normalMode = require("regex-rename.normalMode")
 function M.rename()
     local result = normalMode.rename()
     --print(table.concat(result, ',')
-    vim.pretty_print(result)
 end
 
 return M
