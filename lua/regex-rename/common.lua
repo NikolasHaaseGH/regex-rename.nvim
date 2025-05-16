f954660c7bc73b0d625853e99298596b3091a5ee
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-16 23:54:11 +0200

    asdf

--- lua/regex-rename/common.lua
+++ lua/regex-rename/common.lua
@@ -8,25 +8,27 @@ local function scanLineForMatches(token, line, matchesArray, lineNumber)
 
     local lineLength = #line
     local tokenLength = #token
-
+    local count = 0
     for i = 1, lineLength do
         if lineLength - i < tokenLength then
             break
         end
 
         if line[i] == token[match_step] then
-            if match_step == #token then
+            if match_step == tokenLength then
                 table.insert(matchesArray, {lineNumber, match_start})
-                match_start = i
+                match_start = i+1
                 match_step = 1 
+                count = count + 1
             else
                 match_step = match_step + 1
             end
         else
-            match_start = i
+            match_start = i+1
             match_step = 1
         end
     end
+    print(count)
 end
 
 function M.scanFileForMatches(token, start_line, end_line)
