fe084409e113dfac0bffe69ea517d122a14122d9
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-16 23:35:36 +0200

    asdf

--- lua/regex-rename/init.lua
+++ lua/regex-rename/init.lua
@@ -2,9 +2,22 @@ local M = {}
 
 local normalMode = require("regex-rename.normalMode")
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 function M.rename()
     local result = normalMode.rename()
-        print(result[1])
+        print(dump(result))
 end
 
 return M
