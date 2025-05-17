d576d5ed8688c94bf077a275a5eaf13e67ff4115
nikolas <nikolas@nikolass-MacBook-Pro.local>
2025-05-17 13:24:40 +0200

    asdf[A

--- lua/regex-rename/common.lua
+++ lua/regex-rename/common.lua
@@ -49,7 +49,7 @@ end
 function M.scanFileForMatches(token, start_line, end_line)
     local matches = {}
     local buffer = vim.api.nvim_get_current_buf()
-    local bufferLines = vim.api.nvim_buf_get_lines(buffer, 1, 5, false)
+    local bufferLines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
     local lineColumn = start_line
 
     local s = ""
